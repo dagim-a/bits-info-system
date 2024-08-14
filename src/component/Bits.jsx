3c46c1d455a4e801d31931c2fb72036a6a1d9d06
dagim-a <adgoligndagim@gmail.com>
2024-08-14 13:57:10 +0300

    first commit

---
+++ src/component/Bits.jsx
@@ -0,0 +1,15 @@
+// function Bits(){
+//     return(
+//         <div className="header">
+//         <div style={{display: "flex" }}>
+//             <h2>BITS</h2>
+//             <h3>STUDENT INFORMATION MANAGEMENT </h3>
+//             <h2>SYSTEM</h2>
+//         </div>\
+//         </div>
+//     )
+
+
+
+// }
+// export default Bits
